78d2ee73eca96e1a4cb456fc4cf9fb1281909c0d
lubomirkamensky <lubomir.kamensky@gmail.com>
2023-09-16 19:14:14 +0200

    work in progress

--- hexagram.js
+++ hexagram.js
@@ -2,6 +2,7 @@ const response = await fetch('./iting.json');
 const json = await response.json();
 
 document.getElementById('hex_name').innerHTML = json.Hexagrams["777777"]["name"];
+document.getElementById('hex_id').innerHTML = json.Hexagrams["777777"]["id"];
 document.getElementById('hex_top').innerHTML = json.Hexagrams["777777"]["top"];
 document.getElementById('hex_bottom').innerHTML = json.Hexagrams["777777"]["bottom"];
 document.getElementById('hex_meaning').innerHTML = json.Hexagrams["777777"]["meaning"];
