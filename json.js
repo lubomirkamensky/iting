393e6cf8151b9e9de9f8b0d0d1b0c511afcea38a
lubomirkamensky <lubomir.kamensky@gmail.com>
2023-09-16 17:41:19 +0200

    work in progress

--- json.js
+++ json.js
@@ -1,4 +1,4 @@
 const response = await fetch('./iting.json');
 const json = await response.json();
 
-document.getElementById('main').innerHTML = json["777777"]["name"];
+document.getElementById('main').innerHTML = json.Hexagrams;
