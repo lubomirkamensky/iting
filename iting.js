ceeafad49230ec608e1825387ad9dba99ec1b285
lubomirkamensky <lubomir.kamensky@gmail.com>
2023-09-16 17:47:08 +0200

    work in progress

--- iting.js
+++ iting.js
@@ -1,4 +1,4 @@
 const response = await fetch('./iting.json');
 const json = await response.json();
 
-document.getElementById('main').innerHTML = json.Hexagrams;
+document.getElementById('main').innerHTML = json.Hexagrams["777777"];
