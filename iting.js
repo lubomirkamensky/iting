94dce782799f1b4dea08d866e08a23a5255b2412
lubomirkamensky <lubomir.kamensky@gmail.com>
2023-11-01 16:56:04 +0100

    work in progress

--- iting.js
+++ iting.js
@@ -1,7 +1,40 @@
 const response = await fetch('./iting.json');
 const json = await response.json();
 
+window.currentThrow = [];
+window.allThrows = [];
+window.coins = "";
+window.hexagram = "";
+window.prediction1 = "";
+window.prediction2 = "";
+
+
+function tcoins() {
+	Math.floor((Math.random() * 4) + 2);
+}
+
+function arrSum(arr) { 
+  sum = 0;  
+  arr.forEach(n => sum += n); 
+  return sum; 
+}
+
+
 export function throwCoins() {
+  
+  window.currentThrow[0] = tcoins() ;
+  window.currentThrow[1] = tcoins() ;
+  window.currentThrow[2] = tcoins() ;
+
+  if (window.allThrows.length < 6) {
+    window.allThrows.push(arrSum(window.currentThrow));
+} else {
+    window.allThrows = [];
+    window.allThrows.push(arrSum(window.currentThrow)); 
+}
+
+  
+
   document.getElementById('hex_name').innerHTML = json.Hexagrams["777777"]["name"];
   document.getElementById('hex_id').innerHTML = json.Hexagrams["777777"]["id"];
   document.getElementById('hex_top').innerHTML = json.Hexagrams["777777"]["top"];
