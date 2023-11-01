c52660d19df648ded885340cdb03e6a3e5cd70b7
lubomirkamensky <lubomir.kamensky@gmail.com>
2023-11-01 14:02:28 +0100

    work in progress

--- hexagram.js
+++ hexagram.js
@@ -1,8 +1,12 @@
 const response = await fetch('./iting.json');
 const json = await response.json();
 
-document.getElementById('hex_name').innerHTML = json.Hexagrams["777777"]["name"];
-document.getElementById('hex_id').innerHTML = json.Hexagrams["777777"]["id"];
-document.getElementById('hex_top').innerHTML = json.Hexagrams["777777"]["top"];
-document.getElementById('hex_bottom').innerHTML = json.Hexagrams["777777"]["bottom"];
-document.getElementById('hex_meaning').innerHTML = json.Hexagrams["777777"]["meaning"];
+function throwCoins() {
+  document.getElementById('hex_name').innerHTML = json.Hexagrams["777777"]["name"];
+  document.getElementById('hex_id').innerHTML = json.Hexagrams["777777"]["id"];
+  document.getElementById('hex_top').innerHTML = json.Hexagrams["777777"]["top"];
+  document.getElementById('hex_bottom').innerHTML = json.Hexagrams["777777"]["bottom"];
+  document.getElementById('hex_meaning').innerHTML = json.Hexagrams["777777"]["meaning"];
+}
+
+
